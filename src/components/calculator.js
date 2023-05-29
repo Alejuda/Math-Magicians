3553561928bacb5e18d794aaf9f52ff1b9b84ada
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-05-29 15:10:40 -0300

    Fix eslint error

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,30 +1,28 @@
 import CalcNumber from './CalcNumber';
 
-const Calculator = () => {
-  return (
-    <div className="grid-container">
-      <div className="calc-number calc-screen">0</div>
-      <div className="calc-number calc-res">AC</div>
-      <div className="calc-number calc-toggle-positive">+/-</div>
-      <div className="calc-number calc-percent">%</div>
-      <div className="calc-number calc-op calc-div">÷</div>
-      <CalcNumber number={7} />
-      <CalcNumber number={8} />
-      <CalcNumber number={9} />
-      <div className="calc-number calc-op calc-multi">x</div>
-      <CalcNumber number={4} />
-      <CalcNumber number={5} />
-      <CalcNumber number={6} />
-      <div className="calc-number calc-op calc-rest">-</div>
-      <CalcNumber number={1} />
-      <CalcNumber number={2} />
-      <CalcNumber number={3} />
-      <div className="calc-number calc-op calc-sum">+</div>
-      <div className="calc-number calc-0">0</div>
-      <div className="calc-number calc-coma">.</div>
-      <div className="calc-number calc-op calc-iqual">=</div>
-    </div>
-  );
-}
+const Calculator = () => (
+  <div className="grid-container">
+    <div className="calc-number calc-screen">0</div>
+    <div className="calc-number calc-res">AC</div>
+    <div className="calc-number calc-toggle-positive">+/-</div>
+    <div className="calc-number calc-percent">%</div>
+    <div className="calc-number calc-op calc-div">÷</div>
+    <CalcNumber number={7} />
+    <CalcNumber number={8} />
+    <CalcNumber number={9} />
+    <div className="calc-number calc-op calc-multi">x</div>
+    <CalcNumber number={4} />
+    <CalcNumber number={5} />
+    <CalcNumber number={6} />
+    <div className="calc-number calc-op calc-rest">-</div>
+    <CalcNumber number={1} />
+    <CalcNumber number={2} />
+    <CalcNumber number={3} />
+    <div className="calc-number calc-op calc-sum">+</div>
+    <div className="calc-number calc-0">0</div>
+    <div className="calc-number calc-coma">.</div>
+    <div className="calc-number calc-op calc-iqual">=</div>
+  </div>
+);
 
 export default Calculator;
