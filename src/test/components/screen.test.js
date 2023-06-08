da9851f734e01b0192a74c5d231f149edfc459b7
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-08 13:09:43 -0600

    Pass required params to screen component in test

--- src/test/components/screen.test.js
+++ src/test/components/screen.test.js
@@ -6,11 +6,11 @@ import Screen from '../../components/screen';
 
 describe('The Screen component', () => {
   it('Renders correctly', () => {
-    const tree = renderer.create(<Screen />).toJSON();
+    const tree = renderer.create(<Screen render="0" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('Dwisplays the correct value', () => {
+  it('Displays the correct value', () => {
     render(<Screen render="9" />);
     expect(screen.getByTestId('screen-element')).toHaveTextContent('9');
   });
